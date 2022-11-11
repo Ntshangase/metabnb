279621732f33f7c6131c72d173e35a28f287e625
Ntshangase <simphiwe323i@gmail.com>
2022-11-11 22:24:40 +0200

    added homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
+import Homepage from './homepage.js'
 
 function App() {
   return (
-    null
+    <Homepage />
   );
 }
 
---
+++ src/homepage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function homepage() {
+  return (
+    <div>Welcome to the homepage</div>
+  )
+}

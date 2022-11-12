8b4f3f4fd1a774f8af90543b87c607477d83796a
Ntshangase <simphiwe323i@gmail.com>
2022-11-13 01:38:02 +0200

    nft changes

--- src/components/Nft.js
+++ src/components/Nft.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import learn from '/home/mntwana/metabnb/src/Assets/learn.png';
 
 export default function Nft() {
-  return (
-    <div>Nft</div>
-  )
+    return (
+        <div id='Backnft'>
+            <h2 id='slog2'>Metabnb NFTs</h2>
+            <p>
+                <dl id='para3'>
+                    <dt>Discover our NFT gift cards collection. Loyal</dt>
+                    <dt>customers gets amazing gift cards which are</dt>
+                    <dt>traded as NFTs. These NFTs gives our cutomer</dt>
+                    <dt>access to loads of our exclusive services.</dt>
+                </dl>
+                
+            </p>
+            <img id="lenButton" src={learn} alt='learn' />
+            
+        </div>
+    )
 }

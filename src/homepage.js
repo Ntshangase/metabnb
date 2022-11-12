f7b711829852e0043830fd770955d65910679eb8
Ntshangase <simphiwe323i@gmail.com>
2022-11-12 13:43:13 +0200

    Navbar done

--- src/homepage.js
+++ src/homepage.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import Hhouse from '/home/mntwana/metabnb/src/Assets/Hhouse.png';
-import Hmeta from '/home/mntwana/metabnb/src/Assets/Hmetabnb.png'
+import Navbar from './components/Navbar';
 
 export default function homepage() {
-  return (
-      <div class="navbar"> 
-          <img id="house" src={Hhouse} alt="House" ></img>
-          <img id="meta"  src={Hmeta} alt="Hmeta" ></img>
-
-          <a id="h1" href="#home">Home</a>
-          <a  id='h2' href="#place">Place to Stay</a>
-          <a id="h3" href="#nft">NFTs</a>
-          <a id="h3" href="#community">Community</a>
-      </div>
+  return ( 
+    <Navbar></Navbar>
 
   )
 }

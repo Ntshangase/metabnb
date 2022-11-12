61eb13636018201d4ddce0bc100af338e5be7a46
Ntshangase <simphiwe323i@gmail.com>
2022-11-12 15:58:14 +0200

    added search bar

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,28 @@
 import React from 'react'
 
 export default function Body() {
-  return (
-    <div>
-        <p>
-            <dl>
-                <dt>Rent a Place away from</dt>
-                <dt>Home in the Metaverse</dt>
-            </dl>
-        </p>
-        
-    </div>
-  )
+    return (
+        <div>
+            <p>
+                <dl>
+                    <dt>Rent a Place away from</dt>
+                    <dt>Home in the Metaverse</dt>
+                </dl>
+            </p>
+            <p>
+                <dl>
+                    <dt>we provide you access to luxury and affordable houses</dt>
+                    <dt>in the metaverse get a chance to turn your imagination</dt>
+                    <dt>to reality at your comfort zone</dt>
+                </dl>
+            </p>
+            <p>
+                <form class="example" action="action_page.php">
+                    <input type="text" placeholder="Search.." name="search" />
+                    <button type="submit"><i class="fa fa-search"></i>Search</button>
+                </form>
+            </p>
+
+        </div>
+    )
 }

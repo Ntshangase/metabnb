c8b6ab10edf7f4163e22486d4b152404fb633aeb
Ntshangase <simphiwe323i@gmail.com>
2022-11-12 17:51:59 +0200

    added images1

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import mansion1 from '/home/mntwana/metabnb/src/Assets/mansion1.png';
+import wintercastle1 from '/home/mntwana/metabnb/src/Assets/wintercastle1.png';
+import playground1 from '/home/mntwana/metabnb/src/Assets/playgound1.png';
+import castle1 from '/home/mntwana/metabnb/src/Assets/castle1.png';
 
 export default function Body() {
     return (
         <div>
             <p>
+                <div className="row">
+                    <div className="column">
+                        <img src={mansion1} alt='mansion1' />
+                        <img src={playground1} alt='winercastle1' />
+                    </div>
+                    <div classNaame="column">
+                        <img src={wintercastle1} alt='playground' />
+                        <img src={castle1} alt="castle1" />
+
+                    </div>
+                </div>
                 <dl>
                     <dt>Rent a Place away from</dt>
                     <dt>Home in the Metaverse</dt>
@@ -22,6 +37,7 @@ export default function Body() {
                     <button type="submit"><i class="fa fa-search"></i>Search</button>
                 </form>
             </p>
+
 
         </div>
     )

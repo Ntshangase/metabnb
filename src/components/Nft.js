bb0b8890a2e787c2003930cd1228e592771aeb5b
Ntshangase <simphiwe323i@gmail.com>
2022-11-13 10:43:47 +0200

    Figma is my Friend

--- src/components/Nft.js
+++ src/components/Nft.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import slantframe from '/home/mntwana/metabnb/src/Assets/slantframe.png';
 import learn from '/home/mntwana/metabnb/src/Assets/learn.png';
 
 export default function Nft() {
     return (
         <div id='Backnft'>
             <h2 id='slog2'>Metabnb NFTs</h2>
+            <img id="slant" src={slantframe} alt='slantframe' />
             <p>
                 <dl id='para3'>
                     <dt>Discover our NFT gift cards collection. Loyal</dt>
@@ -12,10 +14,10 @@ export default function Nft() {
                     <dt>traded as NFTs. These NFTs gives our cutomer</dt>
                     <dt>access to loads of our exclusive services.</dt>
                 </dl>
-                
+                <img id="lenButton" src={learn} alt='learnButton' />
             </p>
-            <img id="lenButton" src={learn} alt='learn' />
-            
+
+
         </div>
     )
 }

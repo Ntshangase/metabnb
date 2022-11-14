6790202b1bfe7f5040a78e70b78572f8d0e6d977
Ntshangase <simphiwe323i@gmail.com>
2022-11-14 03:39:43 +0200

    unseen changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,17 @@ import Hmeta from '/home/mntwana/metabnb/src/Assets/Hmetabnb.png';
 
 export default function Navbar() {
   return (
-    <div className="navbar"> 
-    <img id="house" src={Hhouse} alt="House" ></img>
-    <img id="meta"  src={Hmeta} alt="Hmeta" ></img>
+    <div className="navbar">
+      <img id="house" src={Hhouse} alt="House" ></img>
+      <img id="meta" src={Hmeta} alt="Hmeta" ></img>
 
-    <a id="h1" href="#home">Home</a>
-    <a  id='h2' href="#place">Place to Stay</a>
-    <a id="h3" href="#nft">NFTs</a>
-    <a id="h4" href="#community">Community</a>
-    <a id="h5" href="wallet">Connect wallet</a>
-</div>
+      <div id='ancors'>
+        <a id="h1" href="#home">Home</a>
+        <a id='h2' href="#place">Place to Stay</a>
+        <a id="h3" href="#nft">NFTs</a>
+        <a id="h4" href="#community">Community</a>
+      </div>
+      <a id="h5" href="wallet">Connect wallet</a>
+    </div>
   )
 }

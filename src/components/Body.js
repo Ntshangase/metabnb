bac78d135bb58046a76840f40bb774a4343e9c31
Ntshangase <simphiwe323i@gmail.com>
2022-11-14 05:09:06 +0200

    homepage done

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,32 +1,20 @@
 import React from 'react';
-import mansion1 from '/home/mntwana/metabnb/src/Assets/mansion1.png';
-import wintercastle1 from '/home/mntwana/metabnb/src/Assets/wintercastle1.png';
-import playground1 from '/home/mntwana/metabnb/src/Assets/playgound1.png';
-import castle1 from '/home/mntwana/metabnb/src/Assets/castle1.png';
 import frame1 from '/home/mntwana/metabnb/src/Assets/Frame 1.png';
 import Navbar from './Navbar';
+import first4 from '/home/mntwana/metabnb/src/Assets/first4.png';
 
 export default function Body() {
     return (
         <div id="bodypage">
             <Navbar></Navbar>
             <p>
-                <div className="row">
-                    <div className="column">
-                        <img src={mansion1} alt='mansion1' />
-                        <img src={playground1} alt='winercastle1' />
-                    </div>
-                    <div classNaame="column">
-                        <img src={wintercastle1} alt='playground' />
-                        <img src={castle1} alt="castle1" />
-
-                    </div>
-                </div>
+                
                 <dl id='para1'>
                     <dt>Rent a <strong className='hstrong' >Place</strong> away from</dt>
                     <dt> <strong className='hstrong' >Home</strong> in the <strong className='hstrong' >Metaverse</strong></dt>
                 </dl>
             </p>
+            <img id='first4' src={first4} alt='first4' />
             <p>
                 <dl id='para2'>
                     <dt>we provide you access to luxury and affordable houses</dt>

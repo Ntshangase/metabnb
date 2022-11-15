cb5669318c79a79ee5584384d1bd30823c3c1c6a
Ntshangase <simphiwe323i@gmail.com>
2022-11-15 19:13:32 +0200

    Places footer+Header

--- src/components/Places.js
+++ src/components/Places.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Views from '/home/mntwana/metabnb/src/components/Views.js';
+import Footer from './Footer';
+import Navbar from './Navbar';
 
 export default function Places() {
   return (
     <div id="backplaces">
+        <Navbar />
         <Views />
+        <Footer />
     </div>
   )
 }
